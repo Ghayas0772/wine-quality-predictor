e3b8b149ecf8e9f646e57e34399bb531e6702aec
Ghayasudin Ghayas <ghayasudin.edu@gmail.com>
2025-11-15 13:36:55 -0500

    Update HTML layout, app.py and add Dockerfile

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def api_predict():
         return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Listen on all interfaces so Docker can forward the port
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
